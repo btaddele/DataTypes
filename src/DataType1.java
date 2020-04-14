0c1c3e468fdae632f48b9ec3ad0644d8f695fd00
btaddele <Betelhem.biresaw@gmail.com>
2020-04-14 10:12:03 -0400

    first commit

---
+++ src/DataType1.java
@@ -0,0 +1,22 @@
+public class DataType1 {
+    public static void main (String[] args)
+    {
+        boolean flag = true;
+        byte b = 4;
+        char a = 'A';
+        short index = 3;
+        int i = 89;
+        float f = 4.87f;
+        long longvar = 123456789;
+        double doublevar = 123456789.123456789;
+         System.out.println("boolean: " + flag);
+         System.out.println("byte: " + b);
+         System.out.println("char: " + a);
+         System.out.println("short: " + index);
+         System.out.println("float: " + f);
+         System.out.println("long: " + longvar);
+         System.out.println("double: " + doublevar);
+
+
+    }
+}
